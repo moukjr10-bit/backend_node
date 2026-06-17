8f5e0eb0d1b331dc59bd00ed77a1ba6e5c28e796
moukjr10-bit <moukjr10@gmail.com>
2026-06-17 15:36:01 +0000

    commit

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ dotenv.config()
 const app = express();
 connectBD();
 app.use(express.json());
-app.use(cors({ origin: "*"}));
+app.use(cors({
+     origin:[
+        "http://localhost:3000",
+        "https://front-node-azure.vercel.app/"
+     ] 
+     }));
 const PORT = process.env.PORT;
 app.listen( PORT , () => {
     console.log(`serveur démarré sur http://localhost:${PORT}` );
