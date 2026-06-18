7cbec7a10ecd1e058fe1db608da9bccb063a54eb
moukjr10-bit <moukjr10@gmail.com>
2026-06-18 12:38:38 +0000

    commit

---
+++ routes/user.roue.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { inscription , connexion } = require('../controller/user.controller');
+
+const router = express.Router();
+
+router.post('/inscription', inscription);
+router.post('/connexion', connexion)
+
+module.exports = router;
